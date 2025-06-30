1b4261ee42d4e3a4fa7e92381752647d1b3b0c56
lhfzys <taylor_0118@hotmail.com>
2025-06-30 17:48:00 +0800

    feat(infrastructure): 添加 RefreshToken 和 User 的新属性并建立关联
    - 在 RefreshToken 表中添加 Jti 和 UserAgent 列
    - 在 User 表中添加 TenantId 列并建立与 Tenants 表的关联
    - 更新相关实体类以包含新属性

---
+++ F.Fireworks.Infrastructure/Persistence/Migrations/20250630072510_Add_UserAgentAndJti_To_RefreshToken.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace F.Fireworks.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_UserAgentAndJti_To_RefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Jti",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserAgent",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Jti",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "UserAgent",
+                table: "RefreshTokens");
+        }
+    }
+}
---
+++ F.Fireworks.Infrastructure/Persistence/Migrations/20250630081709_Add_Tenant_Nav_To_User.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace F.Fireworks.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Tenant_Nav_To_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TenantId",
+                table: "Users",
+                column: "TenantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Tenants_TenantId",
+                table: "Users",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Tenants_TenantId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_TenantId",
+                table: "Users");
+        }
+    }
+}
